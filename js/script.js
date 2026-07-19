05b1d2148915631951468a0ce20d7de2d9b134f2
unknown <huy.ho210215@vnuk.edu.vn>
2026-07-19 08:45:44 +0700

    . TV Show Details Page

--- js/script.js
+++ js/script.js
@@ -94,6 +94,59 @@ const displayMovieDetails = async () => {
   document.querySelector("#movie-details").appendChild(div);
 };
 
+// Display Show Details
+const displayShowDetails = async () => {
+  const showId = window.location.search.split("=")[1];
+  console.log(showId);
+
+  const show = await fetchAPIData(`tv/${showId}`);
+  //Overlay for background image
+  displayBackgroundImage("tv", show.backdrop_path);
+
+  const div = document.createElement("div");
+  console.log(show);
+  div.innerHTML = `
+    <div class="details-top">
+      <div>
+      ${show.poster_path ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" class="card-img-top" alt="tv Title" />` : `<img src="../images/no-image.jps" class="card-img-top" alt="tv Title" />`}
+      </div>
+      <div>
+        <h2>${show.name}</h2>
+        <p>
+          <i class="fas fa-star text-primary"></i>
+          ${show.vote_average.toFixed(1)} / 10
+        </p>
+        <p class="text-muted">Release Date: ${show.release_date}</p>
+        <p>
+        ${show.overview}
+        </p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+        ${show.genres
+          .map((e) => {
+            return `<li>${e.name}</li>`;
+          })
+          .join("")}
+        </ul>
+        <a href=${show.homepage} target="_blank" class="btn">Visit Show Homepage</a>
+      </div>
+    </div>
+    <div class="details-bottom">
+      <h2>Show Info</h2>
+      <ul>
+        <li><span class="text-secondary">Number Of Episodes:</span> ${show.number_of_episodes}</li>
+        <li>
+          <span class="text-secondary">Last Episode To Air:</span> ${show.last_episode_to_air.name}
+        </li>
+        <li><span class="text-secondary">Status:</span> ${show.status}</li>
+      </ul>
+      <h4>Production Companies</h4>
+      <div class="list-group">${show.production_companies.map((e) => e.name).join(", ")}</div>
+    </div>
+    `;
+  document.querySelector("#show-details").appendChild(div);
+};
+
 // Display Backdrop On Details Pages
 function displayBackgroundImage(type, backgroundPath) {
   const overlayDiv = document.createElement("div");
@@ -176,7 +229,7 @@ function init() {
       displayMovieDetails();
       break;
     case "/tv-details.html":
-      console.log("TV Details");
+      displayShowDetails();
       break;
     case "/search.html":
       console.log("Search");
